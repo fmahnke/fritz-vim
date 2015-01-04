546f94c04426a5253bc2a71a8af29d4d1686d9b1
Fritz Mahnke <fritz@fritzmahnke.com>
2015-01-04 16:00:39 -0700

    Use setup script to install Vundle instead of committing it

--- setup.sh
+++ setup.sh
@@ -11,3 +11,12 @@ do
     cp -R ${var} ~
   fi
 done
+
+if [ -d ~/.vim/bundle/Vundle.vim ]
+then
+  echo "Removing existing Vundle installation."
+  rm -rf ~/.vim/bundle/Vundle.vim
+fi
+
+echo "Cloning Vundle.vim plugin from github."
+git clone git@github.com:gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
